f64461f41821e2cee499496a67c656218426b2d6
coding-fantastic <alexnzioka@protonmail.com>
2020-09-06 17:18:22 +0600

    display agent or installer

--- list.php
+++ list.php
@@ -37,20 +37,23 @@ if (mysqli_num_rows($result) > 0) {
     <table>
 
         <tr>
-            <td>First Name</td>
-            <td>Last Name</td>
-            <td>City</td>
-            <td>Email id</td>
+            <td>#</td>
+            <td>Name</td>
+            <td>Phone</td>
+            <td>Email</td>
+            <td>Identification number</td>
+            <td>Type</td>
         </tr>
         <?php
 $i=0;
 while($row = mysqli_fetch_array($result)) {
 ?>
         <tr>
-            <td><?php echo $row["first_name"]; ?></td>
-            <td><?php echo $row["last_name"]; ?></td>
-            <td><?php echo $row["city_name"]; ?></td>
+            <td><?php echo $row["name"]; ?></td>
+            <td><?php echo $row["phone"]; ?></td>
             <td><?php echo $row["email"]; ?></td>
+            <td><?php echo $row["id"]; ?></td>
+            <td><?php echo $row["type"]; ?></td>
         </tr>
         <?php
 $i++;
