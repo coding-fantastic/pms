b28ed99622b7135229989cda50522c8ed7c4cb7a
coding-fantastic <alexnzioka@protonmail.com>
2020-09-06 19:19:15 +0600

    pdata can now update table

--- insertpdata.php
+++ insertpdata.php
@@ -1,5 +1,7 @@
 <?php
 
+include("database/db_connection.php");
+
 $amountpaid="";
 $startdate="";
 $enddate="";
@@ -22,20 +24,18 @@ $agent=$_POST['agent'];
 $installer=$_POST['installer'];
 $status=$_POST['status'];
 
-echo $agent;
-echo $companyname;
 
-/*
+ 
 $sql = "INSERT INTO pdata (amountpaid, startdate, enddate,companyname,phone1,phone2,phone3,agent,installer,statuss)
 VALUES ('$amountpaid', '$startdate', '$enddate','$companyname','$phone1','$phone2','$phone3','$agent','$installer','$status')";
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($dbcon, $sql)) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-mysqli_close($conn);*/
+mysqli_close($dbcon);
 
 ?>
 
