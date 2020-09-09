e2d295602fb7df560c704620622fbba685be79bb
coding-fantastic <62503960+coding-fantastic@users.noreply.github.com>
2020-09-09 12:13:48 +0300

    update

--- view.php
+++ view.php
@@ -1,47 +1,20 @@
-<html>
 
-<head lang="en">
-    <meta charset="UTF-8">
-    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">
-    <!--css file link in bootstrap folder-->
-    <!-- CSS only -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-    <!-- JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-    <title>View Users</title>
-</head>
-<!--
-<style>
-    .login-panel {
-        margin-top: 150px;
-    }
-
-    .table {
-        margin-top: 50px;
-    }
-
-</style>
--->
-
-
-<body>
-    <div class="container">
+<?php
+ include "header.php";
+?>
 
         <div class="table-scrol">
-            <a href="pdata.php"><button class="btn btn-primary">Companies</button></a>
+            <!--<a href="pdata.php"><button class="btn btn-primary">Companies</button></a>
             <a href="pagent.php"><button class="btn btn-primary">Agent</button></a>
             <a href="pinstaller.php"><button class="btn btn-primary">Installer</button></a>
-            <a href="pdata.php"><button class="btn btn-primary">Home</button></a>
+            <a href="pdata.php"><button class="btn btn-primary">Home</button></a>-->
 
             
             <h1 align="center">List of Companies and their details</h1>
 
             <div class="table-responsive">
                 <!--this is used for responsive display in mobile and other devices-->
-                <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">
+                <table id="datatable" class="table table-bordered table-hover table-striped" style="table-layout: fixed">
                     <thead>
 
                         <tr>
@@ -67,7 +40,7 @@
             include("database/db_connection.php");  
             $view_users_query="select * from company";//select query for viewing users.  
             $run=mysqli_query($dbcon,$view_users_query);//here run the sql query.  
-      
+        
             while($row=mysqli_fetch_array($run))//while look to fetch the result and store in a array $row.  
             {  
                 $id =  $row['company_id'];
@@ -88,8 +61,8 @@
                         <!--here showing results in the table -->
                         <td><?php echo $company_name;  ?></td>
                        <td><?php echo  $amount_paid;  ?></td>
-                <td><?php echo $start_date=$row;  ?></td>
-                <td><?php echo $end_date=$row;  ?></td>
+                <td><?php echo $start_date;  ?></td>
+                <td><?php echo $end_date;  ?></td>
                 
                 <td><?php echo $phone1;  ?></td>
                 <td><?php echo $phone2;  ?></td>
@@ -112,7 +85,7 @@
     Action <i data-feather="chevron-down"></i>
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Edit</a>
+    <a class="dropdown-item" href="epdata.php?id=<?php echo $id; ?>">Edit</a>
     <a class="dropdown-item" href="deletecompany.php?id=<?php echo $id; ?>">Delete</a>
       <a class="dropdown-item" href="suspendcompany.php?id=<?php echo $id; ?>">Suspend</a>
   </div>
@@ -129,10 +102,7 @@
             
         </div>
 
-    </div>
-    
-    
 
-</body>
-
-</html>
+<?php
+ include "footer.php";
+?>
