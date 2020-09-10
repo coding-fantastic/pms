33f6a450326056aab156de3025dce27935116ef1
coding-fantastic <62503960+coding-fantastic@users.noreply.github.com>
2020-09-10 12:00:15 +0300

    tables have a better look

--- view.php
+++ view.php
@@ -1,8 +1,12 @@
-
 <?php
  include "header.php";
+
 ?>
 
+
+<?php  
+          include("database/db_connection.php");  
+?>
 
 
 
@@ -33,45 +37,46 @@
         <div class="card m-b-30">
             <div class="card-body">
 
-        <div class="table-scrol">
-            <!--<a href="pdata.php"><button class="btn btn-primary">Companies</button></a>
+                <div class="table-scrol">
+                    <!--<a href="pdata.php"><button class="btn btn-primary">Companies</button></a>
             <a href="pagent.php"><button class="btn btn-primary">Agent</button></a>
             <a href="pinstaller.php"><button class="btn btn-primary">Installer</button></a>
             <a href="pdata.php"><button class="btn btn-primary">Home</button></a>-->
 
+
+                    <h1 align="center">List of Companies and their details</h1>
+
+                    <div class="table-responsive">
+                        <!--this is used for responsive display in mobile and other devices-->
+                        <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+
+                                <tr>
+                                    <th>#</th>
+                                    <th>Company name</th>
+                                    <th>Amount Paid</th>
+                                    <th>Start date</th>
+                                    <th>End date</th>
+
+                                    <th>Phone 1</th>
+                                    <th>Phone 2</th>
+                                    <th>Phone 3</th>
+                                    <th>Agent</th>
+                                    <th>Installer</th>
+                                    <th>Status</th>
+
+                                    <th>Action</th>
+
+                                </tr>
+                            </thead>
+
+                            <?php  
             
-            <h1 align="center">List of Companies and their details</h1>
-
-            <div class="table-responsive">
-                <!--this is used for responsive display in mobile and other devices-->
-                <table id="datatable" class="table table-bordered table-hover table-striped" style="table-layout: fixed">
-                    <thead>
-
-                        <tr>
-
-                            <th>Company name</th>
-                            <th>Amount Paid</th>
-                            <th>Start date</th>
-                            <th>End date</th>
-                            
-                            <th>Phone 1</th>
-                            <th>Phone 2</th>
-                            <th>Phone 3</th>
-                            <th>Agent</th>
-                            <th>Installer</th>
-                            <th>Status</th>
-                            
-                            <th>Action</th>
-                            
-                        </tr>
-                    </thead>
-
-                    <?php  
-            include("database/db_connection.php");  
-            $view_users_query="select * from company";//select query for viewing users.  
-            $run=mysqli_query($dbcon,$view_users_query);//here run the sql query.  
-        
-            while($row=mysqli_fetch_array($run))//while look to fetch the result and store in a array $row.  
+            $sql="select * from company order by company_id desc";//select query for viewing users.  
+            $result=mysqli_query($dbcon,$sql);//here run the sql query.  
+            if(mysqli_num_rows($result) > 0){
+                $i=1;
+            while($row=mysqli_fetch_array($result))//while look to fetch the result and store in a array $row.  
             {  
                 $id =  $row['company_id'];
                 $amount_paid=$row['amount_paid'];
@@ -87,54 +92,71 @@
       
             ?>
 
-                    <tr>
-                        <!--here showing results in the table -->
-                        <td><?php echo $company_name;  ?></td>
-                       <td><?php echo  $amount_paid;  ?></td>
-                <td><?php echo $start_date;  ?></td>
-                <td><?php echo $end_date;  ?></td>
+                            <tr>
+                                
+                                <!--here showing results in the table -->
+                                <td><?php echo $i; ?></td>
+                                <td><?php echo $company_name;  ?></td>
+                                <td><?php echo  $amount_paid;  ?></td>
+                                <td><?php echo $start_date;  ?></td>
+                                <td><?php echo $end_date;  ?></td>
+
+                                <td><?php echo $phone1;  ?></td>
+                                <td><?php echo $phone2;  ?></td>
+                                <td><?php echo $phone3;  ?></td>
+                                <td><?php echo $agent; 
+                    /*                $sql = "select name from agent2 where agent_id ='$agent'";
+                    $result = mysqli_query($dbcon, $sql);
                 
-                <td><?php echo $phone1;  ?></td>
-                <td><?php echo $phone2;  ?></td>
-                <td><?php echo $phone3;  ?></td>
-                <td><?php echo $agent;  ?></td>
-                <td><?php echo $installer;  ?></td>
-                <td><?php echo $status;  ?></td>
-                        
-                        
-                        <!--<td>
+                while($row=mysqli_fetch_array($result)){
+                    
+                    $agentname = $row['name'];
+                    
+                }*/
+                
+                                    
+                                    ?></td>
+                                <td><?php echo $installer;  ?></td>
+                                <td><?php echo $status;  ?></td>
+
+
+                                <!--<td>
                             <a href=""><button class="btn btn-danger">Delete</button></a>
                             <a href=""><button class="btn btn-primary">edit</button></a>
                             <a href=""><button class="btn btn-primary">Deactivate</button></a>
                         </td>-->
-                        <!--btn btn-danger is a bootstrap button to show danger-->
-                        <td>
-                        
-                            <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle btn-xs" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action <i data-feather="chevron-down"></i>
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="epdata.php?id=<?php echo $id; ?>">Edit</a>
-    <a class="dropdown-item" href="deletecompany.php?id=<?php echo $id; ?>">Delete</a>
-      <a class="dropdown-item" href="suspendcompany.php?id=<?php echo $id; ?>">Suspend</a>
-  </div>
-</div>
-                            
-                        
-                        </td>
-                    </tr>
+                                <!--btn btn-danger is a bootstrap button to show danger-->
+                                <td>
 
-                    <?php } ?>
+                                    <div class="dropdown">
+                                        <button class="btn btn-secondary dropdown-toggle btn-xs" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Action <i data-feather="chevron-down"></i>
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="epdata.php?id=<?php echo $id; ?>">Edit</a>
+                                            <a class="dropdown-item" href="deletecompany.php?id=<?php echo $id; ?>">Delete</a>
+                                            <a class="dropdown-item" href="suspendcompany.php?id=<?php echo $id; ?>">Suspend</a>
+                                        </div>
+                                    </div>
 
-                </table>
-            </div>
-            
-        </div>
 
+                                </td>
+                            </tr>
+
+                            <?php
+                $i++;
+            } 
                 
-                
-                
+            }?>
+
+                        </table>
+                    </div>
+
+                </div>
+
+
+
+
             </div>
         </div>
     </div> <!-- end col -->
