c01fe70e34333497cf78800d6efed975884c187c
coding-fantastic <62503960+coding-fantastic@users.noreply.github.com>
2020-09-10 12:02:40 +0300

    update

--- logout.php
+++ logout.php
@@ -2,5 +2,7 @@
     <?php     
     session_start();//session is a way to store information (in variables) to be used across multiple pages.  
     session_destroy();  
-    header("Location: login.php");//use for the redirection to some page  
+    ?><script>
+window.location="admin_login.php";
+</script><?php
     ?>  
